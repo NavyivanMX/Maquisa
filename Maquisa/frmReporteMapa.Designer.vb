fa1f6f36077201db594766532b929ecfc8d28574
NavyivanMX <narciso_ivan@hotmail.com>
2023-11-28 00:20:10 -0700

    Reporte Mapa Ruta

--- Maquisa/frmReporteMapa.Designer.vb
+++ Maquisa/frmReporteMapa.Designer.vb
@@ -22,6 +22,8 @@ Partial Class frmReporteMapa
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.TBZOOM = New System.Windows.Forms.TrackBar()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.CBTV = New System.Windows.Forms.ComboBox()
@@ -36,14 +38,23 @@ Partial Class frmReporteMapa
         Me.Label2 = New System.Windows.Forms.Label()
         Me.DTDE = New System.Windows.Forms.DateTimePicker()
         Me.BTNMOSTRAR = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.MAPA2 = New GMap.NET.WindowsForms.GMapControl()
+        Me.DGV = New System.Windows.Forms.DataGridView()
         CType(Me.TBZOOM, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'TBZOOM
         '
         Me.TBZOOM.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TBZOOM.LargeChange = 1
-        Me.TBZOOM.Location = New System.Drawing.Point(872, 59)
+        Me.TBZOOM.Location = New System.Drawing.Point(872, 51)
         Me.TBZOOM.Margin = New System.Windows.Forms.Padding(4)
         Me.TBZOOM.Maximum = 17
         Me.TBZOOM.Minimum = 3
@@ -57,7 +68,7 @@ Partial Class frmReporteMapa
         Me.Label9.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label9.AutoSize = True
         Me.Label9.Font = New System.Drawing.Font("Arial Rounded MT Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label9.Location = New System.Drawing.Point(803, 69)
+        Me.Label9.Location = New System.Drawing.Point(803, 61)
         Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(61, 22)
@@ -71,7 +82,7 @@ Partial Class frmReporteMapa
         Me.CBTV.Font = New System.Drawing.Font("Arial Rounded MT Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CBTV.FormattingEnabled = True
         Me.CBTV.Items.AddRange(New Object() {"Camino", "Satelite", "Hibrido", "Terreno"})
-        Me.CBTV.Location = New System.Drawing.Point(872, 9)
+        Me.CBTV.Location = New System.Drawing.Point(872, 1)
         Me.CBTV.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.CBTV.Name = "CBTV"
         Me.CBTV.Size = New System.Drawing.Size(225, 30)
@@ -82,7 +93,7 @@ Partial Class frmReporteMapa
         Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(735, 12)
+        Me.Label1.Location = New System.Drawing.Point(735, 4)
         Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(129, 22)
@@ -95,7 +106,7 @@ Partial Class frmReporteMapa
         Me.CBVEN.Font = New System.Drawing.Font("Arial Rounded MT Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CBVEN.FormattingEnabled = True
         Me.CBVEN.Items.AddRange(New Object() {"Camino", "Satelite", "Hibrido", "Terreno"})
-        Me.CBVEN.Location = New System.Drawing.Point(139, 68)
+        Me.CBVEN.Location = New System.Drawing.Point(144, 60)
         Me.CBVEN.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.CBVEN.Name = "CBVEN"
         Me.CBVEN.Size = New System.Drawing.Size(455, 30)
@@ -105,7 +116,7 @@ Partial Class frmReporteMapa
         '
         Me.LBLVENDEDOR.AutoSize = True
         Me.LBLVENDEDOR.Font = New System.Drawing.Font("Arial Rounded MT Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LBLVENDEDOR.Location = New System.Drawing.Point(11, 71)
+        Me.LBLVENDEDOR.Location = New System.Drawing.Point(16, 63)
         Me.LBLVENDEDOR.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LBLVENDEDOR.Name = "LBLVENDEDOR"
         Me.LBLVENDEDOR.Size = New System.Drawing.Size(120, 22)
@@ -123,7 +134,7 @@ Partial Class frmReporteMapa
         Me.MAPA.GrayScaleMode = False
         Me.MAPA.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow
         Me.MAPA.LevelsKeepInMemory = 5
-        Me.MAPA.Location = New System.Drawing.Point(15, 123)
+        Me.MAPA.Location = New System.Drawing.Point(7, 115)
         Me.MAPA.Margin = New System.Windows.Forms.Padding(4)
         Me.MAPA.MarkersEnabled = True
         Me.MAPA.MaxZoom = 2
@@ -138,7 +149,7 @@ Partial Class frmReporteMapa
         Me.MAPA.ScaleMode = GMap.NET.WindowsForms.ScaleModes.[Integer]
         Me.MAPA.SelectedAreaFillColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(105, Byte), Integer), CType(CType(225, Byte), Integer))
         Me.MAPA.ShowTileGridLines = False
-        Me.MAPA.Size = New System.Drawing.Size(1085, 414)
+        Me.MAPA.Size = New System.Drawing.Size(1090, 394)
         Me.MAPA.TabIndex = 1358
         Me.MAPA.Zoom = 0R
         '
@@ -146,7 +157,7 @@ Partial Class frmReporteMapa
         '
         Me.LBLRUTA.AutoSize = True
         Me.LBLRUTA.Font = New System.Drawing.Font("Arial Rounded MT Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LBLRUTA.Location = New System.Drawing.Point(68, 71)
+        Me.LBLRUTA.Location = New System.Drawing.Point(73, 63)
         Me.LBLRUTA.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LBLRUTA.Name = "LBLRUTA"
         Me.LBLRUTA.Size = New System.Drawing.Size(63, 22)
@@ -159,7 +170,7 @@ Partial Class frmReporteMapa
         Me.CBRUTA.Font = New System.Drawing.Font("Arial Rounded MT Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CBRUTA.FormattingEnabled = True
         Me.CBRUTA.Items.AddRange(New Object() {"Camino", "Satelite", "Hibrido", "Terreno"})
-        Me.CBRUTA.Location = New System.Drawing.Point(139, 68)
+        Me.CBRUTA.Location = New System.Drawing.Point(144, 60)
         Me.CBRUTA.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.CBRUTA.Name = "CBRUTA"
         Me.CBRUTA.Size = New System.Drawing.Size(455, 30)
@@ -169,7 +180,7 @@ Partial Class frmReporteMapa
         '
         Me.RBVEN.AutoSize = True
         Me.RBVEN.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RBVEN.Location = New System.Drawing.Point(14, 24)
+        Me.RBVEN.Location = New System.Drawing.Point(19, 16)
         Me.RBVEN.Name = "RBVEN"
         Me.RBVEN.Size = New System.Drawing.Size(129, 24)
         Me.RBVEN.TabIndex = 1361
@@ -181,7 +192,7 @@ Partial Class frmReporteMapa
         '
         Me.RBRUTA.AutoSize = True
         Me.RBRUTA.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RBRUTA.Location = New System.Drawing.Point(170, 24)
+        Me.RBRUTA.Location = New System.Drawing.Point(175, 16)
         Me.RBRUTA.Name = "RBRUTA"
         Me.RBRUTA.Size = New System.Drawing.Size(79, 24)
         Me.RBRUTA.TabIndex = 1362
@@ -193,7 +204,7 @@ Partial Class frmReporteMapa
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(294, 24)
+        Me.Label2.Location = New System.Drawing.Point(299, 16)
         Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(65, 22)
@@ -205,7 +216,7 @@ Partial Class frmReporteMapa
         Me.DTDE.CalendarFont = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.DTDE.Font = New System.Drawing.Font("Arial Rounded MT Bold", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.DTDE.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
-        Me.DTDE.Location = New System.Drawing.Point(380, 19)
+        Me.DTDE.Location = New System.Drawing.Point(385, 11)
         Me.DTDE.Margin = New System.Windows.Forms.Padding(4)
         Me.DTDE.Name = "DTDE"
         Me.DTDE.Size = New System.Drawing.Size(172, 29)
@@ -217,39 +228,134 @@ Partial Class frmReporteMapa
         Me.BTNMOSTRAR.Font = New System.Drawing.Font("Microsoft Sans Serif", 1.5!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.BTNMOSTRAR.ForeColor = System.Drawing.Color.Transparent
         Me.BTNMOSTRAR.Image = Global.Maquisa.My.Resources.Resources.BTNBUSCAR
-        Me.BTNMOSTRAR.Location = New System.Drawing.Point(617, 9)
+        Me.BTNMOSTRAR.Location = New System.Drawing.Point(622, 1)
         Me.BTNMOSTRAR.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.BTNMOSTRAR.Name = "BTNMOSTRAR"
         Me.BTNMOSTRAR.Size = New System.Drawing.Size(107, 98)
         Me.BTNMOSTRAR.TabIndex = 1365
         Me.BTNMOSTRAR.UseVisualStyleBackColor = False
         '
+        'TabControl1
+        '
+        Me.TabControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1112, 549)
+        Me.TabControl1.TabIndex = 1367
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.RBVEN)
+        Me.TabPage1.Controls.Add(Me.TBZOOM)
+        Me.TabPage1.Controls.Add(Me.BTNMOSTRAR)
+        Me.TabPage1.Controls.Add(Me.Label9)
+        Me.TabPage1.Controls.Add(Me.DTDE)
+        Me.TabPage1.Controls.Add(Me.CBTV)
+        Me.TabPage1.Controls.Add(Me.Label2)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.RBRUTA)
+        Me.TabPage1.Controls.Add(Me.CBVEN)
+        Me.TabPage1.Controls.Add(Me.LBLVENDEDOR)
+        Me.TabPage1.Controls.Add(Me.LBLRUTA)
+        Me.TabPage1.Controls.Add(Me.MAPA)
+        Me.TabPage1.Controls.Add(Me.CBRUTA)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1104, 516)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "General"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.DGV)
+        Me.TabPage2.Controls.Add(Me.MAPA2)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(1104, 516)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Detalle"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'MAPA2
+        '
+        Me.MAPA2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MAPA2.Bearing = 0!
+        Me.MAPA2.CanDragMap = False
+        Me.MAPA2.EmptyTileColor = System.Drawing.Color.Navy
+        Me.MAPA2.GrayScaleMode = False
+        Me.MAPA2.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow
+        Me.MAPA2.LevelsKeepInMemory = 5
+        Me.MAPA2.Location = New System.Drawing.Point(401, 4)
+        Me.MAPA2.Margin = New System.Windows.Forms.Padding(4)
+        Me.MAPA2.MarkersEnabled = True
+        Me.MAPA2.MaxZoom = 2
+        Me.MAPA2.MinZoom = 2
+        Me.MAPA2.MouseWheelZoomEnabled = True
+        Me.MAPA2.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter
+        Me.MAPA2.Name = "MAPA2"
+        Me.MAPA2.NegativeMode = False
+        Me.MAPA2.PolygonsEnabled = True
+        Me.MAPA2.RetryLoadTile = 0
+        Me.MAPA2.RoutesEnabled = True
+        Me.MAPA2.ScaleMode = GMap.NET.WindowsForms.ScaleModes.[Integer]
+        Me.MAPA2.SelectedAreaFillColor = System.Drawing.Color.FromArgb(CType(CType(33, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(105, Byte), Integer), CType(CType(225, Byte), Integer))
+        Me.MAPA2.ShowTileGridLines = False
+        Me.MAPA2.Size = New System.Drawing.Size(696, 505)
+        Me.MAPA2.TabIndex = 1359
+        Me.MAPA2.Zoom = 0R
+        '
+        'DGV
+        '
+        Me.DGV.AllowUserToAddRows = False
+        Me.DGV.AllowUserToDeleteRows = False
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.LightSkyBlue
+        Me.DGV.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle1
+        Me.DGV.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV.Location = New System.Drawing.Point(7, 7)
+        Me.DGV.Margin = New System.Windows.Forms.Padding(4)
+        Me.DGV.Name = "DGV"
+        Me.DGV.ReadOnly = True
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle2.BackColor = System.Drawing.Color.White
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DGV.RowHeadersDefaultCellStyle = DataGridViewCellStyle2
+        Me.DGV.Size = New System.Drawing.Size(386, 502)
+        Me.DGV.TabIndex = 1360
+        '
         'frmReporteMapa
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1113, 550)
-        Me.Controls.Add(Me.BTNMOSTRAR)
-        Me.Controls.Add(Me.DTDE)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.RBRUTA)
-        Me.Controls.Add(Me.RBVEN)
-        Me.Controls.Add(Me.LBLRUTA)
-        Me.Controls.Add(Me.CBRUTA)
-        Me.Controls.Add(Me.MAPA)
-        Me.Controls.Add(Me.LBLVENDEDOR)
-        Me.Controls.Add(Me.CBVEN)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.CBTV)
-        Me.Controls.Add(Me.Label9)
-        Me.Controls.Add(Me.TBZOOM)
+        Me.ClientSize = New System.Drawing.Size(1127, 565)
+        Me.Controls.Add(Me.TabControl1)
         Me.Name = "frmReporteMapa"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Reporte de Rutas"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         CType(Me.TBZOOM, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
@@ -267,4 +373,9 @@ Partial Class frmReporteMapa
     Friend WithEvents Label2 As Label
     Friend WithEvents DTDE As DateTimePicker
     Friend WithEvents BTNMOSTRAR As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents MAPA2 As GMap.NET.WindowsForms.GMapControl
+    Friend WithEvents DGV As DataGridView
 End Class
--- Maquisa/frmReporteMapa.vb
+++ Maquisa/frmReporteMapa.vb
@@ -13,6 +13,9 @@ Public Class frmReporteMapa
     Dim OLINICIO As GMapOverlay
     Dim OLCLIENTE As GMapOverlay
     Dim OVERLAYRUTAS As GMapOverlay
+    Dim OLDETALLE As GMapOverlay
+    Dim DT As New DataTable
+
     Private Sub frmReporteMapa_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         OPVisualizacionForm(Me, frmPrincipal.COLORFUENTE)
         Me.Icon = frmPrincipal.Icon
@@ -24,16 +27,25 @@ Public Class frmReporteMapa
         OLINICIO = New GMapOverlay("MARCAS")
         OLCLIENTE = New GMapOverlay("CLIENTE")
         OVERLAYRUTAS = New GMapOverlay("CLIENTE")
+        OLDETALLE = New GMapOverlay("DETALLE")
 
         LTV.Add(GMapProviders.GoogleMap)
         LTV.Add(GMapProviders.GoogleSatelliteMap)
         LTV.Add(GMapProviders.GoogleHybridMap)
         LTV.Add(GMapProviders.GoogleTerrainMap)
-        MAPA.Overlays.Add(OLINICIO)
+
+
+
+
         CBTV.SelectedIndex = 0
         MAPA.ShowCenter = False
         MAPA.ReloadMap()
         MAPA.Refresh()
+
+        CBTV.SelectedIndex = 0
+        MAPA2.ShowCenter = False
+
+        MAPA2.Refresh()
 
     End Sub
     Private Sub MuestraCombobox()
@@ -53,28 +65,84 @@ Public Class frmReporteMapa
         End If
     End Sub
     Private Sub CARGADATOS()
+        BDEjecutarSql("EXEC SPACTUALIZAICONGEOMARCA @INI,@FIN", frmPrincipal.CadenaConexion, DTDE.Value.Date, DTDE.Value.Date.AddDays(1))
         Dim QUERY As String
         QUERY = ""
         If RBVEN.Checked Then
             If CBVEN.SelectedIndex = 0 Then
                 Return
             End If
-            QUERY = " Select D.FECHA,D.LATITUD,D.LONGITUD,D.Icon From GEOMARCAS D Where VendedorId = " + LVEN(CBVEN.SelectedIndex) + " And FECHA >=@INI And FECHA<@FIN ORDER BY D.FECHA"
+            QUERY = " Select D.FECHA,D.LATITUD,D.LONGITUD,D.Icon,TTT=T.NOMBRE +' '+ISNULL(C.NOMBRE,'')+' '+CONVERT(VARCHAR(10),D.FECHA,108)
+From GEOMARCAS D INNER JOIN TIPOSCHECK T
+ON D.TipoCheckId=T.Clave LEFT JOIN CLIENTES C
+ON D.ClientId=C.Clave Where VendedorId = " + LVEN(CBVEN.SelectedIndex) + " And FECHA >=@INI And FECHA<@FIN ORDER BY D.FECHA"
         End If
         If RBRUTA.Checked Then
             If CBRUTA.SelectedIndex = 0 Then
                 Return
             End If
-            QUERY = "Select D.FECHA,D.LATITUD,D.LONGITUD,D.Icon From GEOMARCAS D Where D.RUTA = " + LRUTA(CBRUTA.SelectedIndex) + " And FECHA >=@INI And FECHA<@FIN ORDER BY D.FECHA"
+            QUERY = "Select D.FECHA,D.LATITUD,D.LONGITUD,D.Icon,TTT=T.NOMBRE +' '+ISNULL(C.NOMBRE,'')+' '+CONVERT(VARCHAR(10),D.FECHA,108)
+From GEOMARCAS D INNER JOIN TIPOSCHECK T
+ON D.TipoCheckId=T.Clave LEFT JOIN CLIENTES C
+ON D.ClientId=C.ClaveWhere D.RUTA = " + LRUTA(CBRUTA.SelectedIndex) + " And FECHA >=@INI And FECHA<@FIN ORDER BY D.FECHA"
         End If
-        Dim DT As New DataTable
-        DT = BDLlenaTabla(QUERY, frmPrincipal.CadenaConexion, DTDE.Value.Date, DTDE.Value.Date.AddDays(1))
-        OLINICIO.Markers.Clear()
 
+        DT = BDLlenaTabla(QUERY, frmPrincipal.CadenaConexion, DTDE.Value.Date, DTDE.Value.Date.AddDays(1))
+
+
+        DGV.DataSource = BDLlenaTabla(QUERY, frmPrincipal.CadenaConexion, DTDE.Value.Date, DTDE.Value.Date.AddDays(1))
+        DGV.Columns(3).Visible = False
+        DGV.Columns(2).Visible = False
+        DGV.Columns(1).Visible = False
+        DGV.Columns(0).Visible = False
+        DgvAjusteEncabezado(DGV, 4)
+
+        DGV.Refresh()
+        EnrutarGrid()
+    End Sub
+
+    Private Sub EnrutarGrid()
+        OLINICIO.Markers.Clear()
+        OLCLIENTE.Markers.Clear()
+        OVERLAYRUTAS.Routes.Clear()
         Dim x As Integer
-        For x = 0 To DT.Rows.Count - 1
-            OLINICIO.Markers.Add(New GMap.NET.WindowsForms.Markers.GMarkerGoogle(New PointLatLng(DT.Rows(x).Item(1), DT.Rows(x).Item(2)), Icon2GMarker(DT.Rows(x).Item(3))))
+        If DGV.RowCount < 2 Then
+            Exit Sub
+        End If
+
+        Dim TTotal, DTotal As Double
+        TTotal = 0
+        DTotal = 0
+        For x = 0 To DGV.RowCount - 2
+            Dim PIni As New PointLatLng
+            Dim PFin As New PointLatLng
+            PIni.Lat = DGV.Item(1, x).Value
+            PIni.Lng = DGV.Item(2, x).Value
+
+            PFin.Lat = DGV.Item(1, x + 1).Value
+            PFin.Lng = DGV.Item(2, x + 1).Value
+            Dim ss As New GDirections
+            GoogleMapProvider.Instance.ApiKey = My.Settings.GoogleMapProviderApiKey
+            GMapProviders.GoogleMap.GetDirections(ss, PIni, PFin, False, False, False, False, False)
+            TTotal += CType(ss.DurationValue, Double)
+            DTotal += CType(ss.DistanceValue, Double)
+            Dim GMR As New GMapRoute(ss.Route, "Mi Ruta")
+            GMR.Duration = ss.DurationValue
+            OVERLAYRUTAS.Routes.Add(GMR)
+
+            OLCLIENTE.Markers.Add(New GMap.NET.WindowsForms.Markers.GMarkerGoogle(New PointLatLng(PIni.Lat, PIni.Lng), Icon2GMarker(DGV.Item(3, x).Value)))
+            OLCLIENTE.Markers(x).ToolTipText = vbNewLine + DGV.Item(4, x).Value.ToString + vbNewLine + " T: " + ss.DurationValue.ToString + vbNewLine + " D: " + ss.DistanceValue.ToString
+
         Next
+        OLCLIENTE.Markers.Add(New GMap.NET.WindowsForms.Markers.GMarkerGoogle(New PointLatLng(DGV.Item(1, DGV.RowCount - 1).Value, DGV.Item(2, DGV.RowCount - 1).Value), Icon2GMarker(DGV.Item(3, x).Value)))
+        OLCLIENTE.Markers(DGV.RowCount - 1).ToolTipText = vbNewLine + DGV.Item(4, x).Value.ToString + vbNewLine + " Ultimo registro " + vbNewLine + "TT: " + TTotal.ToString + vbNewLine + " DT: " + DTotal.ToString
+        'OLCLIENTE.Markers(DGV.RowCount - 1).ToolTipMode = MarkerTooltipMode.Always
+        MAPA.Overlays.Add(OLINICIO)
+        MAPA.Overlays.Add(OLCLIENTE)
+        MAPA.Overlays.Add(OVERLAYRUTAS)
+        MAPA.ZoomAndCenterRoutes("Mi Ruta")
+        MAPA.RoutesEnabled = True
+        MAPA.Refresh()
     End Sub
 
     Private Sub RefreshMap(ByVal GMP As GMap.NET.MapProviders.GMapProvider, ByVal ZOOM As Integer)
@@ -86,6 +154,13 @@ Public Class frmReporteMapa
         MAPA.Manager.Mode = AccessMode.ServerAndCache
         MAPA.Position = New PointLatLng(My.Settings.EmpresaLatitud, My.Settings.EmpresaLongitud)
         PosicionaEmpresa()
+
+        MAPA2.MapProvider = GMP
+        MAPA2.MinZoom = 3
+        MAPA2.MaxZoom = 17
+        MAPA2.Zoom = ZOOM
+        MAPA2.CanDragMap = True
+        MAPA2.Manager.Mode = AccessMode.ServerAndCache
     End Sub
     Private Sub PosicionaEmpresa()
         OLINICIO.Markers.Add(New GMap.NET.WindowsForms.Markers.GMarkerGoogle(New PointLatLng(My.Settings.EmpresaLatitud, My.Settings.EmpresaLongitud), GMarkerGoogleType.red_dot))
@@ -103,7 +178,10 @@ Public Class frmReporteMapa
     End Sub
 
     Private Sub BTNMOSTRAR_Click(sender As Object, e As EventArgs) Handles BTNMOSTRAR.Click
-        CARGADATOS()
+        If MAPA IsNot Nothing Then
+            CARGADATOS()
+        End If
+
     End Sub
 
     Private Sub CBTV_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CBTV.SelectedIndexChanged
@@ -112,5 +190,27 @@ Public Class frmReporteMapa
 
     Private Sub TBZOOM_Scroll(sender As Object, e As EventArgs) Handles TBZOOM.Scroll
         RefreshMap(LTV(CBTV.SelectedIndex), TBZOOM.Value)
+    End Sub
+
+    Private Sub DGV_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGV.CellContentClick
+        PosicionaDGV()
+    End Sub
+
+    Private Sub DGV_SelectionChanged(sender As Object, e As EventArgs) Handles DGV.SelectionChanged
+        PosicionaDGV()
+    End Sub
+
+    Private Sub PosicionaDGV()
+        If DGV.RowCount <= 0 Then
+            Return
+        End If
+        OLDETALLE.Clear()
+
+
+        OLDETALLE.Markers.Add(New GMap.NET.WindowsForms.Markers.GMarkerGoogle(New PointLatLng(DGV.Item(1, DGV.CurrentRow.Index).Value, DGV.Item(2, DGV.CurrentRow.Index).Value), Icon2GMarker(DGV.Item(3, DGV.CurrentRow.Index).Value)))
+        OLDETALLE.Markers(0).ToolTipText = vbNewLine + DGV.Item(4, DGV.CurrentRow.Index).Value.ToString
+        MAPA2.Overlays.Add(OLDETALLE)
+        MAPA2.RoutesEnabled = True
+        MAPA2.Refresh()
     End Sub
 End Class
