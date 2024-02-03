d1a79ff6cd231ab74603ecf0934372d626c435ef
FRANCISCO BUSTAMANTE <panchito_s@hotmail.com>
2024-02-03 13:20:44 -0700

    Cambio de version  a 1.6

--- Maquisa/Form1.vb
+++ Maquisa/Form1.vb
@@ -33,7 +33,7 @@
         USER = "MAROSILOCAL"
         PASS = "Loco*1234"
         Empresa = 1
-        VERSION = "1.5"
+        VERSION = "1.6"
         Sistema = "MAQUISA"
         COLORFUENTE = My.Settings.COLORDEFAULT
 
